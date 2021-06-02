9503510bb5a4084375c56ec14f7a37cebf6221a0
James.Wise <james.wise@adm.com>
2021-06-02 15:06:37 -0400

    Added initial State

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import React from "react";
+import React, { Component } from "react";
 import CardList from "./CardList";
 import SearchBox from "./SearchBox";
 import { robots } from "./robots";
 
-const App = () => {
-	return (
-		<div className = 'tc'>
-			<h1>RoboFriends</h1>
-			<SearchBox />
-			<CardList robots={robots} />
-		</div>
-	);
-};
+class App extends Component {
+	constructor() {
+		super()
+		this.state = {
+			robots: robots,
+			searchfield: "",
+		};
+	}
+
+	onSearchChange = (event) => {
+		const filteredRobots = this.state.robots.filter(robots => {
+			return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
+		})
+		console.log(filteredRobots)
+	}
+
+	render() {
+		return (
+			<div className="tc">
+				<h1>RoboFriends</h1>
+				<SearchBox searchChange={this.onSearchChange}/>
+				<CardList robots={this.state.robots} />
+			</div>
+		);
+	}
+}
 
 export default App;
