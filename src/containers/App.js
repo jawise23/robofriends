37f517ae75d90e9543c9a3563d8477c5bb72f63d
jawise23 <jawise@me.com>
2021-06-02 19:55:09 -0400

    Cleaning up the code and building the data

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
-import CardList from "./CardList";
+import CardList from "../components/CardList";
 //import { robots } from "./robots";
-import SearchBox from "./SearchBox";
+import SearchBox from "../components/SearchBox";
+import Scroll from "../components/Scroll";
 import "./App.css";
 
 class App extends Component {
@@ -24,26 +25,24 @@ class App extends Component {
 	//this.setState({ robots: users }
 	onSearchChange = (event) => {
 		this.setState({ searchfield: event.target.value });
-		console.log(event.target.value);
 	};
 
 	render() {
-		const filteredRobots = this.state.robots.filter((robots) => {
-			return robots.name
-				.toLowerCase()
-				.includes(this.state.searchfield.toLowerCase());
+		const { robots, searchfield } = this.state;
+		const filteredRobots = robots.filter((robot) => {
+			return robot.name.toLowerCase().includes(searchfield.toLowerCase());
 		});
-		if (this.state.robots.length === 0) {
-			return <h1>Loading</h1>;
-		} else {
-			return (
-				<div className="tc">
-					<h1 className="f1">EMERGING TECHNOLOGY</h1>
-					<SearchBox searchChange={this.onSearchChange} />
+		return !robots.length ? (
+			<h1>Loading</h1>
+		) : (
+			<div className="tc">
+				<h1 className="f1">EMERGING TECHNOLOGY</h1>
+				<SearchBox searchChange={this.onSearchChange} />
+				<Scroll>
 					<CardList robots={filteredRobots} />
-				</div>
-			);
-		}
+				</Scroll>
+			</div>
+		);
 	}
 }
 
